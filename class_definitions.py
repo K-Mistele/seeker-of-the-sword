50f01310d5c4501831761f133e20451d90393f82
amistele <andrew@mistele.com>
2018-02-23 14:09:05 -0500

    Finalized position and modification methods to operate on proper indices. Sanitized methods to convert all inputs to expected type of integers. All methods operate on CARTESIAN PLANE COORDINATES (x,y) from [1,dim] with origin in bottom left corner instead of list indices. Easier to provide, manipulate, etc. inputs: worth the extra work. Removed debugging prints. TL;DR finalized methods, class methods operate using cartesian coordinates.

--- class_definitions.py
+++ class_definitions.py
@@ -15,56 +15,54 @@ class create_table:
     def print_tile(self,): # printing the tile
         for row in self.tile:
             print(" ".join(row))
+        print("\n")
 
+    """
+    POSITIONAL FUNCTIONS: RETURN DATA FROM THE TABLE
+    """
     def row(self, index): # getting row by index, INDEXED FROM ONE TO DIM
-        return self.tile[index-1]
-
+        index = int(index)
+        index *= -1
+        return self.tile[index]
     def col(self, index): # getting column by index, INDEXED FROM ONE TO DIM
+        index = int(index)
         column = []
         for row in self.tile:
             column.append(row[index-1])
         return column
+    def char(self, x_index, y_index): # return character by (x,y) coordinates, INDEXED FROM ONE TO DIM
+        x_index = int(x_index)
+        y_index = int(y_index)
+        y_index *= -1
+        return self.tile[y_index][x_index-1]
 
+    """
+    MODIFICATION FUNCTIONS: MODIFY DATA IN THE TABLE
+    """
     def mod_row(self, index, character): # modify all items in a world row to the character
+        index = int(index)
         i = 0
+        index *= -1
         while i < dim:
-            self.tile[index-1][i] = character
+            self.tile[index][i] = character
             i += 1
-
     def mod_col(self, index, character): # modify all items in a world column to the character
+        index = int(index)
         i = 0
         while i < dim:
             self.tile[i][index-1] = character
             i += 1
-
+    def mod_char(self, x_index, y_index, character): # modify character at (x_position, y_position) to the character
+        x_index = int(x_index)
+        y_index = int(y_index)
+        y_index *= -1
+        self.tile[y_index][x_index-1] = character
 
 world = create_table() # creating "world" object in "table" class
-print(world.tile)
+
+world.mod_char(1,1,"1,1")
+world.mod_char(dim,dim,"X")
+world.mod_char(1,dim/2,"Y")
+
 world.print_tile()
 
-world.tile[0][0] = "1,1"
-world.tile[world.tile_dim-1][world.tile_dim-1] = "{},{}".format(dim,dim)
-
-print(world.tile)
-world.print_tile()
-
-"""  ### FOR DEBUGGING AND TESTING 'row', 'col', 'mod_row', and 'mod_col' METHODS ###
-print("Rows:")
-print(world.row(1))
-print(world.row(dim))
-print("\n")
-
-print("Columns:")
-print(world.col(1))
-print(world.col(dim))
-print("\n")
-
-print("Modded row:")
-world.mod_row(2,"K")
-world.print_tile()
-print("\n")
-
-print("Modded column:")
-world.mod_col(2,"P")
-world.print_tile()
-"""
