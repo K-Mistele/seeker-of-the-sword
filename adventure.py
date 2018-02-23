88248e386831bf31ca34ca7578f7984ea967fb9f
amistele <andrew@mistele.com>
2018-02-22 21:50:39 -0500

    Added world thresholds to keep user from crossing over edges of map. User determines world dimensions at beginning of program. Map location and interface smoother and doesn't jump around visually on the console at different steps in the code.

--- adventure.py
+++ adventure.py
@@ -4,10 +4,16 @@ from os import system
 world = []
 game_break = False
 
-#change to increase square dimensions (edge lengths) of the world
+# change to increase square dimensions (edge lengths) of the world
 # world 0-indexed, so world_dim is length but max dim is world_dim-1
-world_dim = 5
+world_dim = int(input("World dimension?"))
 
+# setting up initial board print so that location does'nt shift as commands are input
+
+system("clear")
+print("\n")
+print("\n")
+print(" ")
 # tracking player position
 # player_pos[0] = horizontal coordinate = x
 # player_pos[1] = vertical coordinate = y
@@ -18,6 +24,8 @@ y = 1
 # creating the world
 for i in range(world_dim):
     world.append(["."]*world_dim)
+
+# marking the ORIGIN
 world[0][0] = "O"
 
 # call function to print current tile
@@ -33,17 +41,33 @@ def reset_pos():
 def player_move(motion):
     system("clear")
     if motion == "w":
-        reset_pos()
-        player_pos[y] -= 1
+        if (player_pos[y]-1) > (world_dim-1) or (player_pos[y]-1) < 0:
+            print("You cannot leave the world boundaries!")
+        else:
+            print(" ")
+            reset_pos()
+            player_pos[y] -= 1
     elif motion == "s":
-        reset_pos()
-        player_pos[y] += 1
+        if (player_pos[y]+1) > (world_dim-1) or (player_pos[y]+1) < 0:
+            print("You cannot leave the world boundaries!")
+        else:
+            print(" ")
+            reset_pos()
+            player_pos[y] += 1
     elif motion == "a":
-        reset_pos()
-        player_pos[x] -= 1
+        if (player_pos[x]-1) > (world_dim-1) or (player_pos[x]-1) < 0:
+            print("You cannot leave the world boundaries!")
+        else:
+            print(" ")
+            reset_pos()
+            player_pos[x] -= 1
     elif motion == "d":
-        reset_pos()
-        player_pos[x] += 1
+        if (player_pos[x]+1) > (world_dim-1) or (player_pos[x]+1) < 0:
+            print("You cannot leave the world boundaries!")
+        else:
+            print(" ")
+            reset_pos()
+            player_pos[x] += 1
 
 # actually running the game here
 # constantly getting user input
@@ -64,4 +88,3 @@ while True:
     # print coordinates
     print("coordinates")
     print("({},{})".format(player_pos[x]+1,player_pos[y]+1))
-    print("\n")
