72f3aef002b65e928651f93e336567acd4f1980b
MisteleK <kyle@mistele.com>
2018-03-30 00:00:14 -0400

    Only one cursed shadow should spawn at once. NOTE: necromancer and wizard currently only spawn if name is input as "mob test".

--- entity_classes.py
+++ entity_classes.py
@@ -119,7 +119,7 @@ class monster: # for other monsters to inherit
         if abs(self.x_index - player_x) < self.range or abs(self.y_index - player_y) < self.range: # if player within range
 
             # wizard's range attack
-            if self.name == "Wizard" and (abs(player_x - self.x_index) <= self.attack_range or abs( player_y - self.y_index) <= self.attack_range):
+            if self.name == "Wizard" and (abs(self.x_index - player_x) <= self.attack_range or abs(self.y_index - player_y) <= self.attack_range):
                 player.health -= self.damage
                 print(colorama.Fore.RED + "A wizard casts a magic gaffe at you!" if self.with_colors else "A wizard casts a magic gaffe at you!")
                 # prevent wizard from moving in too close to player
@@ -397,21 +397,21 @@ class necromancer(monster):
                                   self.x_index + 1, self.y_index))
                 self.spawn_cooldown = True
 
-            if (self.does_mob_overlap(self.occupied_tile, self.x_index - 1, self.y_index)[0] == False and
+            elif (self.does_mob_overlap(self.occupied_tile, self.x_index - 1, self.y_index)[0] == False and
                     self.detect_monster_collision_with_world("x", -1, None, targeting=False)[0] == False):
                 self.occupied_tile.monsters.append(
                     cursed_shadow(self.occupied_tile, self.occupied_tile.tile_dim, self.with_colors, True,
                                   self.x_index - 1, self.y_index))
                 self.spawn_cooldown = True
 
-            if (self.does_mob_overlap(self.occupied_tile, self.x_index, self.y_index + 1)[0] == False and
+            elif (self.does_mob_overlap(self.occupied_tile, self.x_index, self.y_index + 1)[0] == False and
                     self.detect_monster_collision_with_world("y", 1, None, targeting=False)[0] == False):
                 self.occupied_tile.monsters.append(
                     cursed_shadow(self.occupied_tile, self.occupied_tile.tile_dim, self.with_colors, True, self.x_index,
                                   self.y_index + 1))
                 self.spawn_cooldown = True
 
-            if (self.does_mob_overlap(self.occupied_tile, self.x_index, self.y_index - 1)[0] == False and
+            elif (self.does_mob_overlap(self.occupied_tile, self.x_index, self.y_index - 1)[0] == False and
                     self.detect_monster_collision_with_world("x", -1, None, targeting=False)[0] == False):
                 self.occupied_tile.monsters.append(
                     cursed_shadow(self.occupied_tile, self.occupied_tile.tile_dim, self.with_colors, True, self.x_index,
