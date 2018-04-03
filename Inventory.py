31673c78319efe54bfcf59685dd486a8d5d3a276
Michael N <soccer72msn2@gmail.com>
2018-04-03 15:41:45 -0400

    TL;DR; Inventory
    Creating a class inventory  to handle the player inventory ect...

---
+++ Inventory.py
@@ -0,0 +1,12 @@
+import inventory_classes
+
+class Inventory:
+    def __init__(self):
+        self.
+        return None
+    #
+
+    #Use a potion ... pass other args
+    def use(self,potionName="",passTheseArgs=[]):
+        return False
+
